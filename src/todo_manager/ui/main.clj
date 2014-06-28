421220b300db16aa0c98d472baefd4356d1940d0
filareta <filareta_yordanova@abv.bg>
2014-06-29 01:33:34 +0300

    Make listing and deleting todos consistent with radio button selection

--- src/todo_manager/ui/main.clj
+++ src/todo_manager/ui/main.clj
@@ -12,10 +12,12 @@
 (def frame (s/frame :title "TODO Manager" :height 640 :width 480
                     :resizable? true :visible? true :on-close :exit))
 
-(def statuses (s/flow-panel :items [(s/checkbox :text "new" :id :new :class :status)
-                                   (s/checkbox :text "in-progress" :id :in-progress :class :status)
-                                   (s/checkbox :text "completed" :id :completed :class :status)
-                                   (s/checkbox :text "all" :id :all :selected? true :class :status)]
+(def button-group (s/button-group))
+
+(def statuses (s/flow-panel :items [(s/radio :text "new" :id :new :group button-group)
+                                   (s/radio :text "in-progress" :id :in-progress :group button-group)
+                                   (s/radio :text "completed" :id :completed :group button-group)
+                                   (s/radio :text "all" :id :all :selected? true :group button-group)]
                            :align :center
                            :hgap 20 :vgap 20))
 
@@ -35,44 +37,58 @@
                          :vgap 10
                          :hgap 20))
 
+(defn resolve-todos-type
+  []
+  (let [status (s/id-of (s/selection button-group))]
+    (if (= status :all)
+      (-> []
+          (into @todos-in-progress)
+          (into @new-todos)
+          (into @completed-todos))
+      @(status status-mapper))))
+
 (defn attach-todo-listeners
   [delete-button edit-button todo]
   (s/listen delete-button :action (fn [e] (delete-todo todo status-mapper)
-                                    (s/config! panel :items (-> []
-                                                                (into @todos-in-progress)
-                                                                (into @new-todos)
-                                                                (into @completed-todos)
+                                    (s/config! panel :items (-> (resolve-todos-type)
                                                                 (draw-collection)
                                                                 (conj top))))))
 
 (defn attach-status-listeners
   []
-  (let [class-selector (s/select statuses [:.status])
-        new-selector (s/select statuses [:#new])
+  (let [new-selector (s/select statuses [:#new])
         in-progress-selector (s/select statuses [:#in-progress])
-        completed-selector (s/select statuses [:#completed])]
+        completed-selector (s/select statuses [:#completed])
+        all-selector (s/select statuses [:#all])]
     (s/listen new-selector
             :action
             (fn [e]
-              (s/config! class-selector :selected? false)
-              (s/config! new-selector :selected? true)
+              (s/selection! button-group new-selector)
               (s/config! panel :items (-> @new-todos
                                           (draw-collection)
                                           (conj top)))))
     (s/listen in-progress-selector
             :action
             (fn [e]
-              (s/config! class-selector :selected? false)
-              (s/config! in-progress-selector :selected? true)
+              (s/selection! button-group in-progress-selector)
               (s/config! panel :items (-> @todos-in-progress
                                           (draw-collection)
                                           (conj top)))))
     (s/listen completed-selector
             :action
             (fn [e]
-              (s/config! class-selector :selected? false)
-              (s/config! completed-selector :selected? true)
+              (s/selection! button-group completed-selector)
               (s/config! panel :items (-> @completed-todos
+                                          (draw-collection)
+                                          (conj top)))))
+    (s/listen all-selector
+            :action
+            (fn [e]
+              (s/selection! button-group all-selector)
+              (s/config! panel :items (-> []
+                                          (into @todos-in-progress)
+                                          (into @new-todos)
+                                          (into @completed-todos)
                                           (draw-collection)
                                           (conj top)))))))
 
@@ -92,21 +108,19 @@
 
 (defn draw-todo
   [{status :status start_date :start_date end_date :end_date goal :goal
-    priority :priority progress :progress tags :tags :as todo} id]
+    priority :priority progress :progress tags :tags :as todo}]
   (let [delete-button (s/button :text "Delete"
-                                :id id
                                 :halign :center
                                 :valign :center
                                 :class :delete)
         edit-button (s/button :text "Edit"
-                              :id id
                               :halign :center
                               :valign :center
                               :class :edit)]
     (attach-todo-listeners delete-button edit-button todo)
-    (s/vertical-panel :items [(s/label :text goal :id id
-                                     :h-text-position :center
-                                     :v-text-position :center)
+    (s/vertical-panel :items [(s/label :text goal
+                                       :h-text-position :center
+                                       :v-text-position :center)
                             (s/progress-bar :orientation :horizontal
                                             :value (* 100 progress))
                             (s/label :text (str "Start date: " (unparse-time start_date)))
@@ -115,6 +129,10 @@
                             (s/label :text (str "Tags: " (join ", " tags)))
                             delete-button edit-button])))
 
+(defn draw-collection
+  [todos]
+  (map draw-todo todos))
+
 (defn draw
   [todos]
   (let [items (draw-collection todos)]
@@ -122,10 +140,3 @@
     (-> frame
       (s/config! :content (s/config! panel :items (conj items top)))
       (s/show!))))
-
-(defn draw-collection
-  [todos]
-  (let [counter 0
-        items (for [todo todos]
-                (draw-todo todo (inc counter)))]
-    items))
